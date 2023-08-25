495fbc7842e37815181a5e70468f59365ca2d16d
Vadhavana Vrajesh Nileshkumar <119277811+VrajeshVadhavana@users.noreply.github.com>
2023-08-25 14:45:43 +0530

    finish

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 9090;
+var fs = require("fs");
+var Formidable = require("formidable");
+app.set("view engine","hbs");
+app.use(express.json());
+app.get("/",(req,res)=>{
+    res.render("index");
+});
+var op,np;
+app.post("/post_data",(req,res)=>{
+    var Form = new Formidable.IncomingForm();
+    Form.parse(req,(err,emp,obj)=>{
+        if(err){
+            console.log("Error");
+        }
+        else{
+            op = obj.u_file[0].filepath;
+            np = __dirname+"/uploads/"+obj.u_file[0].originalFilename;
+            fs.rename(op,np,(err)=>{
+                if(err){
+                    console.log("Error");
+                }
+                else{
+                    console.log("file uploaded successfully");
+                    res.render("index")
+                }
+            })
+        }
+    })
+})
+app.listen(port,(err)=>{
+    if(err){
+        console.log("Error");
+    }
+    else{
+        console.log("http://localhost:"+port);
+    }
+});
